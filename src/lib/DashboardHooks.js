630147239023de66c0fdad7991b2a1f6c87f8602
AoDude <bdistin@gmail.com>
2018-04-04 17:12:32 -0500

    messy middleware for now

--- src/lib/DashboardHooks.js
+++ src/lib/DashboardHooks.js
@@ -2,7 +2,10 @@ const { APIServer } = require('http-nextra');
 
 class DashboardHooks extends APIServer {
 
-	constructor(client, options = { port: 3000 }) {
+	constructor(client, options = {
+		port: 3000,
+		origin: '*'
+	}) {
 		super(async (request, response) => {
 			if (!await this.router.runPath(request.url.slice(1).split('/'), request, response, {})) {
 				response.end('Hello!');
@@ -11,21 +14,23 @@ class DashboardHooks extends APIServer {
 
 		this.client = client;
 
-		this.router.get('api/guilds/', (request, response) => response.end(JSON.stringify(this.client.guilds.keyArray())));
+		this.origin = options.origin;
 
-		this.router.get('api/guilds/:guildID', (request, response, { guildID }) => {
+		this.router.get('api/guilds', this.setHeaders.bind(this), (request, response) => response.end(JSON.stringify(this.client.guilds.keyArray())));
+
+		this.router.get('api/guilds/:guildID', this.setHeaders.bind(this), (request, response, { guildID }) => {
 			const guild = this.client.guilds.get(guildID);
 			if (!guild) response.end('{}');
 			return response.end(JSON.stringify(guild));
 		});
 
-		this.router.get('api/guilds/:guildID/members', (request, response, { guildID }) => {
+		this.router.get('api/guilds/:guildID/members', this.setHeaders.bind(this), (request, response, { guildID }) => {
 			const guild = this.client.guilds.get(guildID);
 			if (!guild) response.end('[]');
 			return response.end(JSON.stringify(guild.members.keyArray()));
 		});
 
-		this.router.get('api/guilds/:guildID/members/:memberID', (request, response, { guildID, memberID }) => {
+		this.router.get('api/guilds/:guildID/members/:memberID', this.setHeaders.bind(this), (request, response, { guildID, memberID }) => {
 			const guild = this.client.guilds.get(guildID);
 			if (!guild) return response.end('{}');
 			const member = guild.members.get(memberID);
@@ -33,13 +38,13 @@ class DashboardHooks extends APIServer {
 			return response.end(JSON.stringify(member));
 		});
 
-		this.router.get('api/guilds/:guildID/roles', (request, response, { guildID }) => {
+		this.router.get('api/guilds/:guildID/roles', this.setHeaders.bind(this), (request, response, { guildID }) => {
 			const guild = this.client.guilds.get(guildID);
 			if (!guild) response.end('[]');
 			return response.end(JSON.stringify(guild.roles.keyArray()));
 		});
 
-		this.router.get('api/guilds/:guildID/roles/:roleID', (request, response, { guildID, roleID }) => {
+		this.router.get('api/guilds/:guildID/roles/:roleID', this.setHeaders.bind(this), (request, response, { guildID, roleID }) => {
 			const guild = this.client.guilds.get(guildID);
 			if (!guild) return response.end('{}');
 			const role = guild.members.get(roleID);
@@ -47,13 +52,13 @@ class DashboardHooks extends APIServer {
 			return response.end(JSON.stringify(role));
 		});
 
-		this.router.get('api/guilds/:guildID/channels', (request, response, { guildID }) => {
+		this.router.get('api/guilds/:guildID/channels', this.setHeaders.bind(this), (request, response, { guildID }) => {
 			const guild = this.client.guilds.get(guildID);
 			if (!guild) response.end('[]');
 			return response.end(JSON.stringify(guild.channels.keyArray()));
 		});
 
-		this.router.get('api/guilds/:guildID/channels/:channelID', (request, response, { guildID, channelID }) => {
+		this.router.get('api/guilds/:guildID/channels/:channelID', this.setHeaders.bind(this), (request, response, { guildID, channelID }) => {
 			const guild = this.client.guilds.get(guildID);
 			if (!guild) return response.end('{}');
 			const channel = guild.members.get(channelID);
@@ -62,9 +67,9 @@ class DashboardHooks extends APIServer {
 		});
 
 		for (const [name, store] of this.client.pieceStores) {
-			this.router.get(`api/${name}/`, (request, response) => response.end(JSON.stringify(store.keyArray())));
+			this.router.get(`api/${name}`, this.setHeaders.bind(this), (request, response) => response.end(JSON.stringify(store.keyArray())));
 
-			this.router.get(`api/${name}/:id`, (request, response, { id }) => {
+			this.router.get(`api/${name}/:id`, this.setHeaders.bind(this), (request, response, { id }) => {
 				const piece = store.get(id);
 				if (!piece) response.end('{}');
 				return response.end(JSON.stringify(piece));
@@ -72,6 +77,12 @@ class DashboardHooks extends APIServer {
 		}
 
 		this.listen(options.port);
+	}
+
+	setHeaders(req, res) {
+		res.setHeader('Access-Control-Allow-Origin', this.origin);
+		res.setHeader('Content-Type', 'application/json');
+		return true;
 	}
 
 }
