b0b1cf31075006c7e44688925710f8dce60d98b7
Unknown <bdistin@gmail.com>
2018-06-17 22:52:35 -0500

    add docs

--- src/lib/structures/DashboardUser.js
+++ src/lib/structures/DashboardUser.js
@@ -1,20 +1,82 @@
 const { Collection } = require('discord.js');
 const DashboardGuild = require('./dashboardGuild');
 
+/**
+ * Represents an OAuth User
+ */
 class DashboardUser {
 
+	/**
+	 * @since 0.0.1
+	 * @param {DashboardClient} client The client
+	 * @param {Object} user The raw user data
+	 */
 	constructor(client, user) {
+		/**
+		 * The DashboardClient
+		 * @since 0.0.1
+		 * @type {DashboardClient}
+		 */
 		this.client = client;
+
+		/**
+		 * The id of the OAuth User
+		 * @since 0.0.1
+		 * @type {string}
+		 */
 		this.id = user.id;
+
+		/**
+		 * The username of the OAuth User
+		 * @since 0.0.1
+		 * @type {string}
+		 */
 		this.username = user.username;
+
+		/**
+		 * The discriminator of the OAuth User
+		 * @since 0.0.1
+		 * @type {number}
+		 */
 		this.discriminator = parseInt(user.discriminator);
+
+		/**
+		 * The language of the OAuth User
+		 * @since 0.0.1
+		 * @type {string}
+		 */
 		this.locale = user.locale;
+
+		/**
+		 * If the OAuth User has multi-factor Authentication enabled
+		 * @since 0.0.1
+		 * @type {boolean}
+		 */
 		this.mfaEnabled = user.mfa_enabled;
+
+		/**
+		 * The OAuth User's avatar hash
+		 * @since 0.0.1
+		 * @type {string}
+		 */
 		this.avatar = user.avatar;
+
+		/**
+		 * The collection of OAuth Guilds this OAuth User is in
+	 * @since 0.0.1
+		 * @type {external:Collection<external:snowflake, DashboardGuild>}
+		 */
 		this.guilds = new Collection();
+
 		this.constructor.setupGuilds(this, user.guilds);
 	}
 
+	/**
+	 * The Avatar URL for this OAuth User
+	 * @since 0.0.1
+	 * @type {string}
+	 * @readonly
+	 */
 	get avatarURL() {
 		const { user } = this;
 		if (user) return user.displayAvatarURL();
@@ -22,14 +84,21 @@ class DashboardUser {
 		return `https://cdn.discordapp.com/embed/avatars/${this.discriminator % 5}.png`;
 	}
 
+	/**
+	 * The User for this OAuth User
+	 * @since 0.0.1
+	 * @type {external:KlasaUser}
+	 * @readonly
+	 */
 	get user() {
 		return this.client.users.get(this.id) || null;
 	}
 
-	static setupGuilds(dashboardUser, guilds) {
-		for (const guild of guilds) dashboardUser.guilds.set(guild.id, new DashboardGuild(dashboardUser.client, guild));
-	}
-
+	/**
+	 * The toJSON behavior of this Object
+	 * @since 0.0.1
+	 * @returns {DashboardUserJSON}
+	 */
 	toJSON() {
 		const user = (usr => usr ? usr.toJSON() : {})(this.user);
 		return {
@@ -43,6 +112,18 @@ class DashboardUser {
 			guilds: [...this.guilds.values()]
 		};
 	}
+
+	/**
+	 * Setups all DashboardGuilds for the passed in Dashboard User
+	 * @since 0.0.1
+	 * @param {DashboardUser} dashboardUser The dashboard user guilds are being setup for
+	 * @param {Object[]} guilds The raw guild data to setup
+	 * @returns {void}
+	 */
+	static setupGuilds(dashboardUser, guilds) {
+		for (const guild of guilds) dashboardUser.guilds.set(guild.id, new DashboardGuild(dashboardUser.client, guild, dashboardUser));
+	}
+
 
 }
 
