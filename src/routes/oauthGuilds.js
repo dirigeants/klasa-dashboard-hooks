ebb5ba120818e0045e234011bc31067d9fb53656
gc <30398469+gach@users.noreply.github.com>
2018-09-13 04:36:53 +1000

    ensure that the member and guild exists, and check that they canManage

--- src/routes/oauthGuilds.js
+++ src/routes/oauthGuilds.js
@@ -12,12 +12,29 @@ module.exports = class extends Route {
 
 	async post(request, response) {
 		const botGuild = this.client.guilds.get(request.body.id);
+		const member = await botGuild.members.fetch(request.auth.scope[0]);
+		const canManage = member.permissions.has('MANAGE_GUILD');
+
+		if (!member || !botGuild) this.notFound(response);
+
+		if (!canManage) return this.unauthorized(response);
+
 		const updated = await botGuild.settings.update(request.body.data, { action: 'overwrite' });
 		const errored = Boolean(updated.errors.length);
 
 		if (errored) this.client.emit('error', `${botGuild.name}[${botGuild.id}] failed updating guild configs via dashboard with error:\n${inspect(updated.errors)}`);
 
 		return response.end(RESPONSES.UPDATED[Number(!errored)]);
+	}
+
+	unauthorized(response) {
+		response.writeHead(401);
+		return response.end(RESPONSES.UNAUTHORIZED);
+	}
+
+	notFound(response) {
+		response.writeHead(404);
+		return response.end(RESPONSES.NOT_FOUND);
 	}
 
 };
