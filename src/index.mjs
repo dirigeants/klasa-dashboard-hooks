36ab3c33339ad3bc23c76f60f5f4581ab48f9de1
kyranet <kyradiscord@gmail.com>
2018-09-17 22:48:30 +0200

    feat: Add ESM exports

---
+++ src/index.mjs
@@ -0,0 +1,14 @@
+import kdh from './index.js';
+import { KlasaClient } from 'klasa';
+
+export const { DashboardClient } = kdh;
+export const { Client } = kdh;
+export const { Server } = kdh;
+export const { Middleware } = kdh;
+export const { MiddlewareStore } = kdh;
+export const { Route } = kdh;
+export const { RouteStore } = kdh;
+export const { constants } = kdh;
+export const { util } = kdh;
+export const { Util } = kdh;
+export const { [KlasaClient.plugin]: plugin } = kdh.Client;
