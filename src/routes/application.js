99973c9948df96cff3945e6eb3282c7d9ed8e4cc
bdistin <bdistin@gmail.com>
2018-11-18 17:45:57 -0600

    fixes #18

--- src/routes/application.js
+++ src/routes/application.js
@@ -14,7 +14,7 @@ module.exports = class extends Route {
 			channels: this.client.channels.size,
 			shards: this.client.options.shardCount,
 			uptime: Duration.toNow(Date.now() - (process.uptime() * 1000)),
-			latency: this.client.ping.toFixed(0),
+			latency: this.client.ws.ping.toFixed(0),
 			memory: process.memoryUsage().heapUsed / 1024 / 1024,
 			invite: this.client.invite,
 			...this.client.application
